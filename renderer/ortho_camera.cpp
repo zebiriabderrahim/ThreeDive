1c4a218423d5eb6549fdc36e45b4ad7c53e3aacc
Abderrahim Zebiri <abderrahim.zebiri@polymtl.ca>
2024-07-06 23:03:40 -0400

    Refactored variables names and there inits

--- renderer/ortho_camera.cpp
+++ renderer/ortho_camera.cpp
@@ -1,23 +1,20 @@
-//
-// Created by ABDERRAHIM ZEBIRI on 2024-06-30.
-//
-
+#include <glm/gtc/matrix_transform.hpp>
 #include "ortho_camera.h"
 
 namespace s3Dive {
 
-    OrthoCamera::OrthoCamera(float left, float right, float bottom, float top)
-            : view_(1.0f),
-              projection_(glm::ortho(left, right, bottom, top, -1.0f, 1.0f)),
-              position_(0.0f){
-    }
+    OrthoCamera::OrthoCamera(float left, float right, float bottom, float top):
+    projectionMatrix_(glm::ortho(left, right, bottom, top, -1.0f, 1.0f)){}
 
-    void OrthoCamera::updateView() {
-        glm::mat4 transform = glm::translate(glm::mat4(1.0f), position_);
-        transform *=glm::rotate(glm::mat4(1.0f),
-                                glm::radians(m_Rotation),
-                                glm::vec3(0, 0, 1));
-        view_ = glm::inverse(transform);
+    void OrthoCamera::updateView()
+    {
+        glm::mat4 transform = glm::translate(glm::mat4(1.0f), position_) *
+                              glm::rotate(glm::mat4(1.0f),
+                                          glm::radians(rotation_),
+                                          glm::vec3(0, 0, 1)
+                                          );
+
+        viewMatrix_ = glm::inverse(transform);
     }
 
 } // s3Dive
--- renderer/ortho_camera.h
+++ renderer/ortho_camera.h
@@ -1,37 +1,38 @@
 //
 // Created by ABDERRAHIM ZEBIRI on 2024-06-30.
 //
+#pragma once
 
 #ifndef THREEDIVE_ORTHO_CAMERA_H
 #define THREEDIVE_ORTHO_CAMERA_H
 
 #include <glm/glm.hpp>
-#include <glm/gtc/quaternion.hpp>
-
 #include "ICamera.h"
-
 
 namespace s3Dive {
 
-    class OrthoCamera: public ICamera{
+    class OrthoCamera final : public ICamera {
 
     public:
         OrthoCamera(float left, float right, float bottom, float top);
+        ~OrthoCamera() override = default;
 
-        void setPosition(const glm::vec3 &position) override { position_ = position; updateView(); }
-        void setRotation(float rotation) override { m_Rotation = rotation; updateView(); }
+        void setPosition(const glm::vec3& position) override { position_ = position; updateView(); }
+        void setRotation(float rotation) override { rotation_ = rotation; updateView(); }
 
-        [[nodiscard]] const glm::vec3 &getPosition() const override { return position_; }
-        [[nodiscard]] float getRotation() const override { return m_Rotation; }
-        [[nodiscard]] const glm::mat4 &getViewMatrix() const override { return view_; }
-        [[nodiscard]] const glm::mat4 &getProjectionMatrix() const override { return projection_; }
+        [[nodiscard]] const glm::vec3& getPosition() const override { return position_; }
+        [[nodiscard]] float getRotation() const override { return rotation_; }
+        [[nodiscard]] const glm::mat4& getViewMatrix() const override { return viewMatrix_; }
+        [[nodiscard]] const glm::mat4& getProjectionMatrix() const override { return projectionMatrix_; }
+
+    protected:
+        void updateView() override;
 
     private:
-        void updateView() override;
-        glm::mat4 view_{};
-        glm::mat4 projection_{};
-        glm::vec3 position_{};
-        float m_Rotation = 0.0f;
+        glm::mat4 viewMatrix_{1.0f};
+        glm::mat4 projectionMatrix_{1.0f};
+        glm::vec3 position_{0.0f};
+        float rotation_{0.0f};
     };
 
 } // s3Dive
