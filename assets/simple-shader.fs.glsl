e7010dc6db8bab31fa16a5fdcff284d700ad64f2
Abderrahim Zebiri <abderrahim.zebiri@polymtl.ca>
2024-07-29 22:53:11 -0400

    Added default material shaders class

--- assets/simple-shader.fs.glsl
+++ assets/simple-shader.fs.glsl
@@ -1,52 +1,46 @@
 #version 330 core
-
 out vec4 FragColor;
 
 in vec3 FragPos;
 in vec3 Normal;
 in vec2 TexCoords;
-in vec3 VertexColor;
+
+struct Material {
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+	float shininess;
+};
+
+uniform Material material;
+uniform bool useTexture;
+uniform sampler2D texture_diffuse1;
 
 uniform vec3 lightPos;
 uniform vec3 viewPos;
-uniform vec3 lightColor;
-uniform vec3 objectColor;
-uniform float specularStrength;
-uniform bool useTexture;
-uniform bool useVertexColor;
-uniform bool isWireframe;
-uniform sampler2D texture_diffuse1;
 
 void main()
 {
-	// Ambient
-	float ambientStrength = 0.1;
-	vec3 ambient = ambientStrength * lightColor;
-
-	// Diffuse
 	vec3 norm = normalize(Normal);
 	vec3 lightDir = normalize(lightPos - FragPos);
-	float diff = max(dot(norm, lightDir), 0.0);
-	vec3 diffuse = diff * lightColor;
-
-	// Specular
 	vec3 viewDir = normalize(viewPos - FragPos);
 	vec3 reflectDir = reflect(-lightDir, norm);
-	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-	vec3 specular = specularStrength * spec * lightColor;
 
-	vec3 result;
-	if (useTexture) {
-		result = (ambient + diffuse + specular) * texture(texture_diffuse1, TexCoords).rgb;
-	} else if (useVertexColor) {
-		result = (ambient + diffuse + specular) * VertexColor;
-	} else {
-		result = (ambient + diffuse + specular) * objectColor;
-	}
+	// Ambient
+	vec3 ambient = material.ambient;
 
-	if (isWireframe) {
-		FragColor = vec4(0.0, 0.0, 0.0, 1.0); // Black wireframe
-	} else {
-		FragColor = vec4(result, 1.0);
-	}
+	// Diffuse
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 diffuse = diff * material.diffuse;
+
+	// Specular
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+	vec3 specular = material.specular * spec;
+
+	vec3 result = ambient + diffuse + specular;
+
+	if (useTexture)
+	result *= texture(texture_diffuse1, TexCoords).rgb;
+
+	FragColor = vec4(result, 1.0);
 }
